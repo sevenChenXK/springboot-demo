7cf13b0a9749cfd9a5a070e49409ab51583c0b35
zhizhonghua <zhizhonghua@boco.com.cn>
2018-03-01 00:22:47 +0800

    创建 springboot-demo-activemq 工程

---
+++ springboot-demo-ativemq/src/main/java/net/cc/springboot/demo/ativemq/SpringbootDemoAtivemqApplication.java
@@ -0,0 +1,12 @@
+package net.cc.springboot.demo.ativemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoAtivemqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoAtivemqApplication.class, args);
+	}
+}
